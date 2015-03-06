894beef1a3d0a51a16db38620ec149551030b360
jasper-d <none@none>
2015-03-06 19:13:15 +0100

    Clean up some code, also set preferred label (shelf mark) for all languages used for cataloguing.

--- app/plugins/ShelfMarkGen/ShelfMarkGenPlugin.php
+++ app/plugins/ShelfMarkGen/ShelfMarkGenPlugin.php
@@ -14,25 +14,35 @@ require_once(__CA_MODELS_DIR__ . "/ca_object_labels.php");
 require_once(__CA_MODELS_DIR__ . "/ca_locales.php");
 require_once("ShelfMarkService.php");
 
+/**
+ * Class ShelfMarkGenPlugin
+ * Plugin to automatically generate shelf marks (e.g. E-INF-G001)
+ * and set item source (SLUB/WISE), based on bar code attribute
+ */
+//TODO: Set item source
 class ShelfMarkGenPlugin extends BaseApplicationPlugin
 {
 	# -------------------------------------------------------
 	/**
-	 *
+	 * Plugin description
 	 */
 	protected $description = null;
 
 	/**
-	 *
+	 * Plugin config
 	 */
 	private $opo_config;
 
 	/**
-	 *
+	 * Plugin path
 	 */
 	private $ops_plugin_path;
 
 	# -------------------------------------------------------
+	/**
+	 * Default constructor
+	 * @param $ps_plugin_path string Path to plugin
+	 */
 	public function __construct($ps_plugin_path)
 	{
 		$this->ops_plugin_path = $ps_plugin_path;
@@ -43,109 +53,53 @@ class ShelfMarkGenPlugin extends BaseApplicationPlugin
 		$this->opo_config = Configuration::load($ps_plugin_path . "/conf/shelfmarkgen.conf");
 	}
 
-/*	public function hookSaveItem(&$args)
+	# -------------------------------------------------------
+	/**
+	 * Hook is called by template every time a item or relationship is inserted.
+	 * Not the best extension point for what we do, but the only one where we have all information we need.
+	 * Used to set the shelf mark of copies
+	 */
+	public function hookAfterBundleInsert(&$args)
 	{
-		$item = $args["instance"];
-		$vn_type_id = $item->get($item->getTypeFieldName());
-		$va_type_list = $item->getTypeList(array("directChildrenOnly" => false, "returnHierarchyLevels" => false, "item_id" => null));
-		$type = $va_type_list[$vn_type_id];
-		if ($type["idno"] === "copy") {
-			$relatedItems = $item->get("ca_objects.related.object_id", array("returnAsArray" => true));
-			$shelfMark = $item->get("ca_objects.preferred_labels");
-			$barcode = $item->get("ca_objects.barcode");
-			$sourceId = $item->get("ca_objects.source_id");
-			$idno = $item->get("ca_objects.idno");
-			$listItems = new ca_list_items();
-			$slubSourceId = $listItems->find(array("idno" => "obj_src_slub"));
-
-			$item->setMode(ACCESS_WRITE);
-
-		}
-	}*/
-
-	public function hookAfterBundleInsert(&$args){
-		if($args["table_name"] !== "ca_objects_x_objects"){
+		if ($args["table_name"] !== "ca_objects_x_objects") {
 			return;
 		}
 
 		$relationship = $args["instance"];
 		$book = new ca_objects($relationship->get("object_left_id"));
 		$copy = new ca_objects($relationship->get("object_right_id"));
-		$copyId = $copy->get("ca_objects.object_id");
 
-		if($book->getTypeCode() !== "book" || $copy->getTypeCode() !== "copy"){
+		if ($book->getTypeCode() !== "book" || $copy->getTypeCode() !== "copy") {
 			return;
 		}
 
 		//Appears that a shelf mark is already set
-		if (preg_match('/^\w-\w{1,10}-\w+$/', $copy->get("ca_objects.preferred_labels.name"))){
+		if (preg_match('/^\w-\w{1,10}-\w+$/', $copy->get("ca_objects.preferred_labels.name"))) {
 			return;
 		}
 
 		$author = $book->get("ca_entities", array("returnAsArray" => true, "restrictToRelationshipTypes" => array("main_book_author")));
 		$authorSurname = reset($author)["surname"];
 		$category = $book->get("ca_list_items.value", array("restrictToRelationshipTypes" => "book_category"));
-		if($category === ""){
+		if ($category === "") {
 			//TODO: Remove debug code
 			$category = $book->get("ca_list_items.idno", array("restrictToRelationshipTypes" => "book_category"));
 		}
-		$shelfmarkService = new ShelfMarkService();
-		$shelfmark = $shelfmarkService->getShelfmark($authorSurname, $category);
+		$shelfMarkService = new ShelfMarkService();
+		$shelfMark = $shelfMarkService->getShelfMark($authorSurname, $category);
 
 		$locales = new ca_locales();
-		$catalogingLocales = $locales->find(array("dont_use_for_cataloguing" => 0), array("returnAs" => "ids"));
+		$cataloguingLocales = $locales->find(array("dont_use_for_cataloguing" => 0), array("returnAs" => "ids"));
 
-		//Won't update otherwise
-		//$timestamp = $_REQUEST['form_timestamp'];
-		//unset($_REQUEST['form_timestamp']);
-		$a1 = $copy->setMode(ACCESS_WRITE);
-		$a11 = $copy->removeAllLabels(__CA_LABEL_TYPE_PREFERRED__);
-		$a2 = $copy->addLabel(array("name" => $shelfmark), 1, null, true);
-		$a3 = $copy->update(array("force" => true, "dontCheckCircularReferences" => true));
-		$a4 = $copy->doSearchIndexing(null, true, null);
-		//$_REQUEST['form_timestamp'] = $timestamp;
-		//NOT WORKING FOR SOME REASON I DON'T UNDERSTAND, FALLING BACK TO RAW SQL
-		/*
-		unset($_REQUEST['form_timestamp']);
-
-		$objectLabels = new ca_object_labels();
-		$copyLabels = $objectLabels->find(array("object_id" => $relationship->get("object_right_id")), array("returnAs" => "firstModelInstance"));
-		$copyLabels->setMode(ACCESS_WRITE);
-		//$copyLabels->set("idno", $shelfmark);
-		$a = $copyLabels->set("name_sort", $shelfmark);
-		$b = $copyLabels->set("name", $shelfmark);
-		$c = $copyLabels->setAsChanged("name_sort");
-		$d = $copyLabels->setAsChanged("name");
-		$e = $copyLabels->update(array("force" => true, "dontCheckCircularReferences" => true));
-		//$u = $copy->update(array("force" => true, "dontCheckCircularReferences" => true));
-		//$f = 1;//$copyLabels->update(array("force" => true, "dontCheckCircularReferences" => true));
-		//BUG: Causes items to not appear in checkout search
-		//$shelfmarkService->updateCopy($copyId, $shelfmark);
-		*/
-	}
-
-/*	public function hookBeforeBundleInsert(&$args){
-		$item = $args["instance"];
-		$vn_type_id = $item->get($item->getTypeFieldName());
-		$va_type_list =  $item->getTypeList(array("directChildrenOnly" => false, 'returnHierarchyLevels' => true, 'item_id' => null));
-		$type = $va_type_list[$vn_type_id];
-		$typeIdno = $type["idno"];
-		if($type["idno"] === "copy") {
-			$preferredLabelName = $item->get("ca_objects.preferred_labels");
-			$preferredLabelName2 = $item->get("preferred_labels.name");
-			$barcode = $item->get("ca_objects.barcode");
-			$sourceId = $item->get("ca_objects.source_id");
-			$relatedItems = $item->get("ca_objects.related.object_id");
-			$sourceName = null;
-			$listItems = new ca_list_items();
-			$slubSourceId = $listItems->find(array("idno" => "obj_src_slub"));
-			$item->set("preferred_labels.name", "mylabel");
-			$item->set("source_id", 27);
-			$preferredLabelNameNew = $item->get("ca_objects.preferred_labels");
-			$sourceIdNew = $item->get("ca_objects.source_id");
-			$foo = 1;
+		$copy->setMode(ACCESS_WRITE);
+		$copy->removeAllLabels(__CA_LABEL_TYPE_PREFERRED__);
+		foreach ($cataloguingLocales as $key => $value) {
+			$copy->addLabel(array("name" => $shelfMark), $value, null, true);
 		}
-	}*/
+
+		$copy->update(array("force" => true, "dontCheckCircularReferences" => true));
+		$copy->doSearchIndexing(null, true, null);
+	}
 
 	# -------------------------------------------------------
 	/**
@@ -164,12 +118,11 @@ class ShelfMarkGenPlugin extends BaseApplicationPlugin
 	# -------------------------------------------------------
 	/**
 	 * Add plugin user actions
+	 * We don't have any custom permissions, but need to fulfill the contract defined by
+	 * IApplicationPlugin.
 	 */
 	static function getRoleActionList()
 	{
 		return array();
 	}
-	# -------------------------------------------------------
 }
-
-?>
--- app/plugins/ShelfMarkGen/ShelfMarkService.php
+++ app/plugins/ShelfMarkGen/ShelfMarkService.php
@@ -11,17 +11,25 @@
 
 class ShelfMarkService {
 	/**
+	 * Gets a shelf mark for a given author and category combination
 	 * @param $authorSurname string Surname of main author
 	 * @param $categoryAbbr string Abbreviation (value) of category
 	 * @return string The generated shelf mark
 	 */
-	public function getShelfmark($authorSurname, $categoryAbbr)
+	public function getShelfMark($authorSurname, $categoryAbbr)
 	{
 		$category = $this->normalizeCategory($categoryAbbr);
 		$firstLetter = $this->getFirstLetterNormalized($authorSurname);
-		return $this->generateShelfMark($firstLetter, $category);
+		$index = $this->generateShelfMark($firstLetter, $category);
+		$shelfMark = $this->formatShelfMark($firstLetter, $category, $index);
+		return $shelfMark;
 	}
 
+	/**
+	 * Get the first letter of a string in normalized form, uppercase
+	 * @param $string string string to get first letter from
+	 * @return string Uppercased, normalized first letter, "X" if $string is empty.
+	 */
 	private function getFirstLetterNormalized($string){
 		if($string === ""){
 			$firstName = "X";
@@ -31,13 +39,25 @@ class ShelfMarkService {
 		return $firstName;
 	}
 
+	/**
+	 * Normalizes category string
+	 * @param $category string category abbreviation
+	 * @return string category abbreviation uppercased
+	 */
 	private function normalizeCategory($category){
-		return mb_strtoupper(mb_substr($category, 0, 3));
+		return mb_strtoupper($category);
 	}
 
+	/**
+	 * Gets the index from a custom SQL table
+	 * @param $firstLetter string First letter of author surname
+	 * @param $category string Category abbreviation
+	 * @return int
+	 */
 	private function generateShelfMark($firstLetter, $category){
 		$maxIndex = null;
 		try {
+			//TODO: Use table locks
 			$con = new PDO(sprintf("mysql:host=%s;dbname=%s", __CA_DB_HOST__, __CA_DB_DATABASE__), __CA_DB_USER__, __CA_DB_PASSWORD__);
 			$con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
@@ -48,12 +68,14 @@ class ShelfMarkService {
 			$result = $cmd->fetchAll(PDO::FETCH_COLUMN, 0);
 			$maxIndex = $result[0];
 			if($maxIndex == null){
+				//We don't have this combination of author (first letter of surname) and category yet
 				$maxIndex = 1;
 				$cmd = $con->prepare("INSERT INTO tud_shelf_marks(author, category, `index`) VALUES(:author, :category, 1);");
 				$cmd->bindParam(":author", $firstLetter);
 				$cmd->bindParam(":category", $category);
 				$cmd->execute();
 			} else {
+				//We have this combination of author (first letter of surname) and category already, increment index by 1
 				$maxIndex++;
 				$cmd = $con->prepare("UPDATE tud_shelf_marks SET `index` = `index` + 1 WHERE author = :author AND category = :category;");
 				$cmd->bindParam(":author", $firstLetter);
@@ -67,32 +89,17 @@ class ShelfMarkService {
 			exit;
 		}
 
-
-
-		return $this->formatShelfMark($firstLetter, $category, $maxIndex);
+		return $maxIndex;
 	}
 
+	/**
+	 * Format shelf mark string
+	 * @param $firstLetter string First letter of author surname
+	 * @param $category string Category abbreviation
+	 * @param $index int Index number
+	 * @return string shelf mark
+	 */
 	private function formatShelfMark($firstLetter, $category, $index){
 		return sprintf("E-%s-%s%s", $category, $firstLetter, str_pad($index, 3, "0", STR_PAD_LEFT));
-	}
-
-	public function updateCopy($copyId, $shelfmark)
-	{
-		/*
-		$maxIndex = null;
-		try {
-			$con = new PDO(sprintf("mysql:host=%s;dbname=%s", __CA_DB_HOST__, __CA_DB_DATABASE__), __CA_DB_USER__, __CA_DB_PASSWORD__);
-			$con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-			$cmd = $con->prepare("UPDATE ca_object_labels SET `name` = :shelfmark, name_sort = :shelfmark WHERE object_id = :objectid;");
-			$cmd->bindParam(":objectid", $copyId);
-			$cmd->bindParam(":shelfmark", $shelfmark);
-			$cmd->execute();
-			$con = null;
-		}
-		catch (PDOException $e) {
-			echo "There was an error! " . $e->getMessage();
-			exit;
-		}
-		*/
 	}
 }
